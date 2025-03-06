a3b0069f23342d3ad1f1f3bee7d2d8a2f70ad1aa
BohdanSapatsinskiy <ipzk241_sbi@student.ztu.edu.ua>
2025-03-06 13:37:14 +0200

    Add files via upload

---
+++ ConsoleToWeb/Program.cs
@@ -0,0 +1,19 @@
+﻿//namespace ConsoleToWeb
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+//}
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "ConsoleToWeb - Hello world!");
+
+app.Run();
