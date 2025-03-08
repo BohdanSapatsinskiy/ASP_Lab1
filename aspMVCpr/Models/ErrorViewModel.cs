3690d1bb905af5e021d1a617b063f8e2de82e081
BohdanSapatsinskiy <ipzk241_sbi@student.ztu.edu.ua>
2025-03-08 10:01:02 +0200

    Delete aspMVCpr directory

--- aspMVCpr/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace aspMVCpr.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
