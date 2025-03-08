e0cef5543a130ca0513953b7ab9e1b646f0f8f15
BohdanSapatsinskiy <ipzk241_sbi@student.ztu.edu.ua>
2025-03-08 10:35:22 +0200

    виправлення помилок з git

---
+++ Controllers/LabController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspMVCpr.Controllers
+{
+    public class LabController : Controller
+    {
+        [HttpGet]
+        public IActionResult Info()
+        {
+            var labInfo = new
+            {
+                LabNumber = "1",
+                Theme = "Вступ до ASP.NET Core",
+                Goal = "Ознайомитися з основними принципами роботи .NET, навчитися налаштовувати середовище розробки та встановлювати необхідні компоненти, набути навичок створення рішень та проектів різних типів, набути навичок обробки запитів з використанням middleware.",
+                Name = "Богдан",
+                Surname = "Сапацінський"
+            };
+
+            return View(labInfo);
+        }
+    }
+}
